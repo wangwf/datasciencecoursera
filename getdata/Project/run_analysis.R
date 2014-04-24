3c9178031ce7edcf0543621088a72464a015e942
Wenfeng Wang <wenfeng@venor.co>
2014-04-23 23:46:41 -0700

    update run_analysis.R

--- getdata/Project/run_analysis.R
+++ getdata/Project/run_analysis.R
@@ -1,63 +1,75 @@
 
 ## download and unzip files
-if(!file.exists("./UCI_HAR_Dataset/")){
-    dir.create("./UCI_HAR_Dataset")
-    dataURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-    temp <-tempfile()
-    download.file(dataURL, temp, method="curl")
-    unzip(temp,exdir=".")
-    ## rename dir-name ""UCI HAR Dataset" to "UCI_HAR_Dataset"
-    # mv UCI\ HAR\ Dataset/ UCI_HAR_Dataset
-    file.rename("UCI HAR Dataset", "UCI_HAR_Dataset")
-    unlink(temp)
+downloadFiles<-function(
+    dataURL="https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    ){
+    if(!file.exists("./UCI_HAR_Dataset/")){
+        dir.create("./UCI_HAR_Dataset")
+        temp <-tempfile()
+        download.file(dataURL, temp, method="curl")
+        unzip(temp,exdir=".")
+        ## rename dir-name ""UCI HAR Dataset" to "UCI_HAR_Dataset"
+        # mv UCI\ HAR\ Dataset/ UCI_HAR_Dataset
+        file.rename("UCI HAR Dataset", "UCI_HAR_Dataset")
+        unlink(temp)
+    }
 }
 
 # subject_*.txt, one of 30 volunteers
 # X_train.txt/X_test.txt, 561-feature vector with time and frequencey domain variable
 # y_train.txt/y_test.txt, 1:6, one of six activities ( (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING))
-# 
 
-features <-read.table("./UCI_HAR_Dataset/features.txt") #,colClasses="character")
-#selectedFeatures<-features[grepl("mean()|std()",features[,2]),]
-#  select feature with mean() or std(),  excluding meanFreq()
-selected<-(grepl("mean()",features[,2],fixed=TRUE)|grepl("std()",features[,2],fixed=TRUE))
-selectedFeatures<-features[selected,]
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+extactFeatures <-function(){
+    features <-read.table("./UCI_HAR_Dataset/features.txt") #,colClasses="character")
+    #selectedFeatures<-features[grepl("mean()|std()",features[,2]),]
+    #  select feature with mean() or std(),  excluding meanFreq()
+    # grepl("mean\\(\\)",features[,2]),
+    selected<-(grepl("mean()",features[,2],fixed=TRUE)
+               |grepl("std()",features[,2],fixed=TRUE))
+    selectedFeatures<-features[selected,]
+}
 
-nsample=1000L #100
-trainSubject <-readLines("./UCI_HAR_Dataset/train//subject_train.txt",n=nsample)
-trainX <- read.table("UCI_HAR_Dataset/train/X_train.txt",nrows=nsample)
-trainY <- read.table("UCI_HAR_Dataset/train/y_train.txt",nrows=nsample)
-if( length(trainSubject) != nrow(trainX)){message("Error: Mismatch between subject_train.txt and X_train.txt")}
-if( length(trainSubject) != nrow(trainY)){message("Error: Mismatch between subject_train.txt and y_train.txt")}
-trainSample <- cbind(trainSubject, trainY, trainX[,selectedFeatures[,1]])
+readData <- function(pathSuffix = "UCI_HAR_Dataset/train", fileSuffix = "train", nsampleSize = -1L){
+    #read subject
+    subject <- read.table(paste0(pathSuffix,"/subject_",fileSuffix,".txt"),nrows=nsampleSize,
+                         col.names=c("subjectID"))
 
-testSubject <-readLines("./UCI_HAR_Dataset/test//subject_test.txt", n=nsample)
-testX <- read.table("UCI_HAR_Dataset/test/X_test.txt", nrows=nsample)
-testY <- read.table("UCI_HAR_Dataset/test/y_test.txt", nrows=nsample)
-## check length of format of subject*.txt and *_train.txt
-if( length(testSubject) != nrow(testX)){message("Error: Mismatch between subject_test.txt and X_test.txt")}
-if( length(testSubject) != nrow(testY)){message("Error: Mismatch between subject_test.txt and y_test.txt")}
+    # read the measurement variables -- features
+    features <- read.table(paste0(pathSuffix,"/../features.txt"),
+                           col.names=c("featureID","featureName"))
 
-testSample <- cbind(testSubject, testY, testX[,selectedFeatures[,1]])
+    #Extracts only the measurements on the mean and standard deviation for each measurement
+    # Note: exclude variables with meanFreq()
+    selectedFeatures <- features[grepl("mean\\(\\)|std\\(\\)", features$featureName),]
 
+    #read X data, and subseting
+    data <- read.table(paste0(pathSuffix,"/X_",fileSuffix,".txt"), nrows=nsampleSize,
+                    col.names=features$featureName)
+    data <- data[,selectedFeatures$featureID]
 
-## 1. Merges the training and the test sets to create one data set
-#mergeX <- rbind(trainX[,selectedFeatures[,1]], testX[,selectedFeatures[,1]]); mergeY <- rbind(trainY, testY)
-mergeSample <- rbind(trainSample, testSample)
+    #read y data -- activities
+    Y <- read.table(paste0(pathSuffix,"/y_",fileSuffix,".txt"), nrows=nsampleSize,
+                col.names=c("activityID"))
+    
+    # append the length of activity and subject
+    if( nrow(subject) != nrow(data)){message("Error: Mismatch between subject_*.txt and X_*.txt")}
+    if( nrow(subject) != nrow(Y)){message("Error: Mismatch between subject_*.txt and y_*.txt")}
+
+    data <- cbind(subject, Y, data)
+    data
+}
+
+mergeData<-function(nsampleSize= -1L){
+    trainData <- readData("UCI_HAR_Dataset/train", "train", nsampleSize)
+    testData  <- readData("UCI_HAR_Dataset/test","test", nsampleSize)
+    mergeData <-rbind(trainData, testData)
+    mergeData
+}
 
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-#means <- lapply(trainX, mean)
-#sds   <- lapply(trainX, sd)
-#mergeX<- mergeX[,selectedFeatures[,1]]  #  mean (x,y,x) and std (x,y,z)
-
 ## 3. Uses descriptive activity names to name the activities in the data set
-
-#newNames <- c("tBodyAccMeanX","tBodyAccMeanY","tBodyAccMeanZ","tBodyAccStdX","tBodyAccStdY","tBodyAccStdZ")
-names(mergeSample) <- c("subject", "activity",selectedFeatures[,2])
-
 ## 4. Appropriately labels the data set with descriptive activity names. 
-#rownames(mergeX) <- as.character(mergeY)
-
 ## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 # write.table(newData)
 
@@ -65,4 +77,26 @@ names(mergeSample) <- c("subject", "activity",selectedFeatures[,2])
 ##Casting data frames
 #cylData <-dcast(carMelt, cyl ~ variable)
 
+activityLabel <- function(data, labelFile="UCI_HAR_Dataset//activity_labels.txt"){
+    activityLabels <- read.table(labelFile,col.names=c("activityID","activityName"))
+    activityLabels$activityName <- as.factor(activityLabels$activityName)
+    dataLabled<-merge(data,activityLabels)
+    dataLabled
+}
 
+getTidyDataset <- function(mergeData){
+    library(reshape2)
+    #melt the dataset
+    ids <- c("activityID","activityName", "subjectID")
+    measureVar <-setdiff(colnames(mergeData), ids)
+    meltedData <- melt(mergeData, iid=ids, measure.vars=measureVar)
+    
+    #casting
+    dcast(meltedData, activityName + subjectID ~variable, mean)
+}
+
+#create a tidy data file and save it
+writeTidyDataFile <-function(outputname="tidydata.txt"){
+    tidyData <- getTidyDataset( activityLabel(mergeData()))
+    write.table(tidyData, outputname)
+}
