5742072db12f32be69e3f2e7c6d32b4a5daa9580
Wenfeng <wenfeng@venor.co>
2014-04-25 14:21:11 -0700

    add more scripts

--- getdata/getdata_q3.R
+++ getdata/getdata_q3.R
@@ -1,35 +1,57 @@
 
 ##question 1
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
-filename = "ss06hid.csv"
-## describing the variable name
-##https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf
+# The American Community Survey distributes downloadable data about United States communities.
+# Download the 2006 microdata survey about housing for the state of Idaho using download.file() from here:   
+#  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv 
+# and load the data into R. The code book, describing the variable names is here:   
+#  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf 
+# Create a logical vector that identifies the households on greater than 10 acres 
+# who sold more than $10,000 worth of agriculture products. Assign that logical vector
+# to the variable agricultureLogical. Apply the which() function like this to identify
+# the rows of the data frame where the logical vector is TRUE. which(agricultureLogical)
+# What are the first 3 values that result?
+Q1 <- function(){
+  url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+  filename = "ss06hid.csv"
+  ## describing the variable name
+  ##https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf
+  if(!file.exists(filename)){
+    download.file(url, destfile=filename, method="curl")
+  }
 
-if(!file.exists(filename)){
-  download.file(url, destfile=filename, method="curl")
+  data <- read.csv(filename)
+  ##the households on greater than 10 acres who sold more than $10,000 worth of agriculture products
+  data[which(data$ACR==3 & data$AGS==6),]
+  head(data[which(data$ACR==3 & data$AGS==6),][,1:12])
 }
 
-data <- read.csv(filename)
-##the households on greater than 10 acres who sold more than $10,000 worth of agriculture products
-data[which(data$ACR==3 & data$AGS==6),]
-head(data[which(data$ACR==3 & data$AGS==6),][,1:12])
+### Q2
+# Using the jpeg package read in the following picture of your instructor into R 
+# https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg 
+# Use the parameter native=TRUE. What are the 30th and 80th quantiles of the resulting data?
+Q2 <- function(){
+  library(jpeg)
+  url2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
+  filename2 <- "jeff.jpg"
+  if(!file.exists(filename2)){
+    download.file(url2, destfile=filename2, method="curl")
+  }
+  data2<-readJPEG(filename2, native=TRUE)
+  quantile(data2,c(0.30,0.80))
 
-
-###
-library(jpeg)
-url2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
-filename2 <- "jeff.jpg"
-if(!file.exists(filename2)){
-  download.file(url2, destfile=filename2, method="curl")
 }
-data2<-readJPEG(filename2, native=TRUE)
-quantile(data2,c(0.30,0.80))
-
 
 ##### Question 3
-## original data sources
-## http://data.worldbank.org/data-catalog/GDP-ranking-table 
-## http://data.worldbank.org/data-catalog/ed-stats
+# Load the Gross Domestic Product data for the 190 ranked countries in this data set:   
+#  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv 
+# Load the educational data from this data set:   
+#  https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv 
+# Match the data based on the country shortcode. How many of the IDs match?
+# Sort the data frame in descending order by GDP rank. What is the 13th country in the resulting data frame? 
+#
+# Original data sources: 
+#  http://data.worldbank.org/data-catalog/GDP-ranking-table 
+# http://data.worldbank.org/data-catalog/ed-stats
 
 url3_1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
 url3_2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
@@ -57,13 +79,14 @@ mergedata[mergedata$ranking==13,1:4]
 mergedata[,2] <- as.numeric(as.character(mergedata[,2]));
 #mergedata[,4] <- as.integer(mergedata[,4])
 #head(mergedata[order(as.numeric(as.character(mergedata$ranking)), decreasing=F),1:5],n=15)
-head(mergedata[order(mergedata$ranking, decreasing=F),1:5],n=15)
+head(mergedata[order(mergedata$ranking, decreasing=F=T),1:5],n=13)
 
 
+# Q4
 #mean(as.numeric(as.character(mergedata[mergedata$Income.Group == "High income: OECD",2])))
 #mean(as.numeric(as.character(mergedata[mergedata$Income.Group == "High income: nonOECD",2])))
 mean(mergedata[mergedata$Income.Group == "High income: OECD",2])
 mean(mergedata[mergedata$Income.Group == "High income: nonOECD",2])
 
-
+# Q5
 table(mergedata[mergedata$ranking<39,]$Income.Group)
--- getdata/getdata_w2.R
+++ getdata/getdata_w2.R
@@ -1,28 +1,29 @@
 library(RMySQL)
 
 ##Example -- UCSC database
+# connect and listing databases
 ucscDb <- dbConnect(MySQL(), user="genome",
                     host="genome-mysql.cse.ucsc.edu")
-result <- dbGetQuery(ucscDb, "show database;");
-dbDisconnect(ucscDb)
+result <- dbGetQuery(ucscDb, "show database;"); dbDisconnect(ucscDb)
+result
 
-#
-hg19 <- dbConnect(MySQL(), user="genome", db="hp19",
+# connecting to hg19 and listing tables
+hg19 <- dbConnect(MySQL(), user="genome", db="hg19",
                   host="genome-mysql.cse.ucsc.edu")
-allTables <- dbListTables(hp19)
+allTables <- dbListTables(hg19)
 length(allTables)
 
 # Get dimensions of a specific table
 dbListFields(hg19, "affyU133Plus2")
+dbGetQuery(hg19, "select count(*) from affyU133Plus2")
 
-dbGetQuery(hp19, "select count(*) from affyU133Plus2")
-
+# read from the table
 affyData <- dbReadTable(hg19, "affyU133Plus2")
 head(affyData)
 
 # Select a specific subset
-query <-dbSendQuery(hpg19, "select * from affyU133Plus2 where misMatches between 1 and 2")
-affMis <- fetch(query);
+query <-dbSendQuery(hg19, "select * from affyU133Plus2 where misMatches between 1 and 2")
+affyMis <- fetch(query);
 quantile(affyMis$misMatches)
 
 affMisSmall <- fetch(query, n=10);
@@ -61,7 +62,6 @@ df = data.frame(1L:5L, seq(0,1, length.out=5),
 h5write(df, "example.h5","df")
 h5ls("example.h5")
 
-
 ### Reading data
 readA = h5read("example.h5", "foo/A")
 readB = h5read("example.h5", "foo/foobaa/B")
@@ -72,6 +72,9 @@ readA
 h5write(c(12, 13, 14), "example.h5", "foo/A", index = list(1:3,1))
 h5read("example.h5", "foo/A")
 
+
+
+### Reading data from the web
 #### WEB api
 ## Getting data off webpages -- readLines()
 con = url("http://scholar.google.com/citations?user=HI-I6C0AAAAJ")
@@ -105,3 +108,19 @@ names(pg2)
 google = handle("http://google.com")
 pg1 = GET(handle=google, path="/")
 pg2 = GET(handle=google, path="search")
+
+
+#### Twitter API ####
+myapp = oauth_app("twitter",
+                  key="LZMG9RxP4hFiDowxYMQ",
+                  secret="Ap8NNB1lEJLb03s25k7QK6HhfiEcnHcOaebn6mpXo0")
+sig = sign_oauth1.0(myapp,
+                    token="369198012-s0YaD6OjfXhKK5yYAdB8JfJkyrMrOMqPSYeh38Kq",
+                    token_secret="rpITlQtCH409EOzKyc0as0HbBLqFuRJoqwIBmPr9P4")
+
+homeTL=GET("https://api.twitter.com/1.1/statuses/home_timeline.json",sig)
+
+
+json1 = content(homeTL)
+json2 = jsonLite::fromJSON(toJSON(json1))
+json2[1,1:4]
